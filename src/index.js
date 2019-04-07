8cf869e7ec306010c3bcaf1a249180cfc46809f0
Jbevvy <jonbeveridge@live.com>
2019-04-07 15:40:43 +0100

    index.js unknown error

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import "./style/main.scss";
 
 class Welcome extends React.Component {
     render () {
-        return (<h1>Test Works!</h1>);
+        return (
+            <>
+            <h1 className="header">Hello World from React boilerplate</h1>
+            <p>Visit my repository at <a href="https://github.com/nsebhastian/my-react-boilerplate">GitHub</a></p>
+            </>
+        );
     }
 }
 
-ReactDOM.render(<Welcome />, document.getElementById('root'));
+ReactDOM.render(<Welcome />, document.getElementById("root"));
